16e36628e76f11245241cc4daf8ef49255c4e6bf
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-05-13 13:48:58 +0200

    mongoose, mongodb: added logging messages and a connection keep-alive mechanism.

--- backend/modules/mongodb.js
+++ backend/modules/mongodb.js
@@ -2,17 +2,22 @@
 
 'use strict';
 
-const mongodb = require('mongodb');
+var mongodb = require('mongodb');
 
 exports.DEFAULT_CONFIG = {
   uri: 'mongodb://127.0.0.1:27017/test',
   server: {},
-  db: {}
+  db: {},
+  keepAliveQueryInterval: 30000
 };
 
 exports.start = function startMongodbModule(app, module, done)
 {
-  mongodb.MongoClient.connect(module.config.uri, module.config, function(err, db)
+  let keepAliveFailed = false;
+
+  mongodb.MongoClient.connect(module.config.uri, module.config, onComplete);
+
+  function onComplete(err, db)
   {
     if (err)
     {
@@ -21,6 +26,53 @@ exports.start = function startMongodbModule(app, module, done)
 
     module.db = db;
 
+    setUpEventListeners();
+    setUpKeepAliveQuery();
+
+    module.debug('Open.');
+
     done();
-  });
+  }
+
+  function setUpEventListeners()
+  {
+    module.db.on('error', (err) => module.error(err.stack));
+    module.db.on('parseError', (err) => module.error(err.stack));
+    module.db.on('timeout', () => module.warn('Timeout.'));
+    module.db.on('close', () => module.warn('Closed.'));
+    module.db.on('reconnect', () => module.debug('Reconnected.'));
+    module.db.on('authenticated', () => module.debug('Authenticated.'));
+  }
+
+  function setUpKeepAliveQuery()
+  {
+    if (!module.config.keepAliveQueryInterval)
+    {
+      return;
+    }
+
+    module.db.stats(function(err, stats)
+    {
+      if (err)
+      {
+        if (!keepAliveFailed)
+        {
+          module.error(`Keep alive query failed: ${err.message}`);
+        }
+
+        keepAliveFailed = true;
+      }
+      else
+      {
+        if (keepAliveFailed)
+        {
+          module.debug(`Kept alive: ${JSON.stringify(stats)}`);
+        }
+
+        keepAliveFailed = false;
+      }
+
+      setTimeout(setUpKeepAliveQuery, module.config.keepAliveQueryInterval);
+    });
+  }
 };
