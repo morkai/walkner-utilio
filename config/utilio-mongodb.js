62681fa0a1134c5d449472d7518e387a49e80061
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2016-05-13 13:49:37 +0200

    utilio-mongodb: changed the connection configuration to avoid the 'topology destruction'.

--- config/utilio-mongodb.js
+++ config/utilio-mongodb.js
@@ -1,15 +1,40 @@
 // Part of <https://miracle.systems/p/walkner-utilio> licensed under <CC BY-NC-SA 4.0>
 
+/* eslint-disable no-process-env */
+
 'use strict';
 
+const url = require('url');
+
+const uri = url.parse(process.env.WMES_MONGODB_URI || 'mongodb://127.0.0.1:27017/walkner-utilio');
+
+if (process.env.UTILIO_MONGODB_USER)
+{
+  uri.auth = process.env.UTILIO_MONGODB_USER;
+
+  if (process.env.UTILIO_MONGODB_PASS)
+  {
+    uri.auth += ':' + process.env.UTILIO_MONGODB_PASS;
+  }
+}
+
 module.exports = {
-  uri: 'mongodb://127.0.0.1:27017/walkner-utilio',
+  uri: url.format(uri),
+  keepAliveQueryInterval: 15000,
   server: {
-    poolSize: 15
+    poolSize: 15,
+    reconnectTries: Number.MAX_SAFE_INTEGER,
+    reconnectInterval: 1000,
+    socketOptions: {
+      autoReconnect: true,
+      keepAlive: 1000,
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 0
+    }
   },
   db: {
     w: 1,
-    wtimeout: 1000,
+    wtimeout: 5000,
     nativeParser: true,
     forceServerObjectId: false
   }
